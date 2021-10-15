3b8ebbb4611a2447086c7b56d7d3decbb0868741
Camilo Gonzalez <jmilo.dev@gmail.com>
2021-10-15 17:00:54 -0300

    fix errors to run code

--- ejercicio1.sql
+++ ejercicio1.sql
@@ -6,14 +6,14 @@ CREATE TABLE cursos (
   turn VARCHAR(20) NOT NULL
 );
 --agrega el campo cupos
-ALTER TABLE cursos ADD cupos
+ALTER TABLE cursos ADD cupo;
 --inserta valores
 INSERT INTO cursos VALUES (101, "Algoritmos","Algoritmos y Estructuras de Datos","Mañana",35),
-  (102, "Matemática Discreta","","Tarde",30)
+  (102, "Matemática Discreta","","Tarde",30);
 --errores
-INSERT INTO cursos VALUES (103, null,"","Tarde",32)
-INSERT INTO cursos VALUES (102, "Fundamentos POO","","Mañana",32)
+  -- INSERT INTO cursos VALUES (103, null,"","Tarde",32)
+  -- INSERT INTO cursos VALUES (102, "Fundamentos POO","","Mañana",32)
 --actualiza el cupo de todos los registros
-UPDATE cursos SET cupo=25
+UPDATE cursos SET cupo=25;
 --elimina el curso algoritmos
 DELETE from cursos where id=101
