e7ae123f730b1d06b875e619d2e57b08b389d941
Camilo Gonzalez <jmilo.dev@gmail.com>
2021-10-22 11:57:17 -0300

    sql-II ejercicio 1

---
+++ SQL -II/tema1.sql
@@ -0,0 +1,29 @@
+--Total de estudiantes en la carrera Mecanica (código 102)
+SELECT COUNT(ESTUDIANTE_legajo) 
+FROM INSCRIPCION 
+WHERE CURSO_codigo = 102;
+
+--salario mínimo de los profesores nacidos en la década del 80
+SELECT MIN(salario)
+FROM PROFESOR
+WHERE fecha_nacimiento BETWEEN '1980-01-01' AND '1989-12-31';
+
+--CONSULTAS A PARTIR DE MODELO RELACIONAL:
+  --Cantidad de pasajeros por país
+  SELECT COUNT(idpasajero) 
+  FROM PASAJERO 
+  GROUP BY idpais
+
+  --Suma de todos los pagos realizados
+  SELECT SUM(monto)
+  FROM PAGO
+
+  --Suma de todos los pagos que realizó un pasajero. El monto debe aparecer con dos decimales
+  SELECT ROUND(SUM(monto),2)
+  FROM PAGO
+  GROUP BY idpasajero
+
+  --Promedio de los pagos que realizó un pasajero.
+  SELECT ROUND(AVG(monto),2)
+  FROM PAGO
+  GROUP BY idpasajero
